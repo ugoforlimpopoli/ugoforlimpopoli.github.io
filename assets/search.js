12c80a6f93ecae632c49951da35e08a6228f176e
niccolozanotti <niccolozanotti@users.noreply.github.com>
2024-09-08 07:07:02 +0000

    deploy: 252e8e72d92c3bb2e65b41f8a28ae07df2e4fd6b

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"TeXt - Extensions",'url':"/2024/09/07/extensions.html"}]};
