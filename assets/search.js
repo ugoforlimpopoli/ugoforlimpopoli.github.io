cc2e7ad2a77227dadba18c5d421c440a51a514a9
niccolozanotti <niccolozanotti@users.noreply.github.com>
2024-09-09 15:12:05 +0000

    deploy: 671aabcf56962fabecdf5a629b6bdaf481a33342

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Un evento",'url':"/2024/09/08/evento.html"},{'title':"Welcome",'url':"/2024/09/08/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Un evento",'url':"/2024/09/08/evento.html"}]};
